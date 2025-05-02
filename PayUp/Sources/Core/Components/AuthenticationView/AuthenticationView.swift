b1b24e65fa896738061812b3c885d8ba029aa7db
arthur <arthur.rios007@gmail.com>
2025-05-01 22:04:23 -0300

    Módulo PayUp - Construindo AuthenticationView

--- PayUp/Sources/Core/Components/AuthenticationView/AuthenticationView.swift
+++ PayUp/Sources/Core/Components/AuthenticationView/AuthenticationView.swift
@@ -16,6 +16,39 @@ final class AuthenticationView: UIView {
        return imageView
     }()
     
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "mainLogo"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = Fonts.titleLarge()
+        label.textColor = Colors.textHeading
+        label.text = "Olá Calisto,"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = Fonts.paragraphMedium()
+        label.textColor = Colors.textParagraph
+        label.text = "Desbloqueie com segurança usando o Face ID."
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let faceIdIcon: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "faceId"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -27,6 +60,10 @@ final class AuthenticationView: UIView {
     
     private func setupView() {
         addSubview(backgroundView)
+        addSubview(logoImageView)
+        addSubview(titleLabel)
+        addSubview(descriptionLabel)
+        addSubview(faceIdIcon)
         
         setupConstraints()
     }
@@ -36,7 +73,22 @@ final class AuthenticationView: UIView {
             backgroundView.topAnchor.constraint(equalTo: topAnchor),
             backgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
             backgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            backgroundView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            backgroundView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            logoImageView.topAnchor.constraint(equalTo: topAnchor, constant: 45),
+            logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 94),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
+            
+            faceIdIcon.centerXAnchor.constraint(equalTo: centerXAnchor),
+            faceIdIcon.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 40),
+            faceIdIcon.widthAnchor.constraint(equalToConstant: 40),
+            faceIdIcon.heightAnchor.constraint(equalToConstant: 40),
         ])
     }
 }
