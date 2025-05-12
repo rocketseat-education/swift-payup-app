1b81c11ea92819b63ef2b021d9422575571f8947
arthur <arthur.rios007@gmail.com>
2025-05-12 14:10:39 -0300

    Módulo PayUp - Concluindo a CollectionCell

--- PayUp/Sources/Core/Components/Company/View/CompanyCell.swift
+++ PayUp/Sources/Core/Components/Company/View/CompanyCell.swift
@@ -19,6 +19,23 @@ final class CompanyCell: UICollectionViewCell {
         return view
     }()
     
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "company"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = Colors.textSpan
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = Fonts.titleSmall()
+        label.textColor = Colors.textHeading
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
@@ -29,8 +46,31 @@ final class CompanyCell: UICollectionViewCell {
     }
     
     private func setupView() {
+        backgroundColor = Colors.backgroundSecondary
+        layer.cornerRadius = 6
+        
+        addSubview(iconBackgroundView)
+        iconBackgroundView.addSubview(iconImageView)
+        addSubview(nameLabel)
+        
         NSLayoutConstraint.activate([
+            iconBackgroundView.topAnchor.constraint(equalTo: topAnchor, constant: 12),
+            iconBackgroundView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            iconBackgroundView.widthAnchor.constraint(equalToConstant: 31),
+            iconBackgroundView.heightAnchor.constraint(equalToConstant: 31),
             
+            iconImageView.centerXAnchor.constraint(equalTo: iconBackgroundView.centerXAnchor),
+            iconImageView.centerYAnchor.constraint(equalTo: iconBackgroundView.centerYAnchor),
+            iconImageView.widthAnchor.constraint(equalToConstant: 24),
+            iconImageView.heightAnchor.constraint(equalToConstant: 24),
+            
+            nameLabel.topAnchor.constraint(equalTo: iconBackgroundView.bottomAnchor, constant: 4),
+            nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12)
         ])
+    }
+    
+    func configure(with model: CompanyItemModel) {
+        nameLabel.text = model.name
     }
 }
